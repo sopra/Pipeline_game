db4a156c6b55549b5a66127d85de48c921fce1ec
tgotou <tgotou@yahoo-corp.jp>
2019-09-29 17:37:37 +0900

    [develop] under development.

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,21 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        pipeline
-      </h1>
-      <h2 class="subtitle">
-        My mathematical Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <PIPETile :pattern="[ [0, 0, 1, 1], [2, 3, 2, 3] ]" />
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import PIPETile from '~/components/molecules/PIPETile'
 
 export default {
+  name: '',
   components: {
-    Logo
+    PIPETile
+  },
+  data () {
+    return {}
   }
 }
 </script>
